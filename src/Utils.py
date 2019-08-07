55803109c8245cde2bab435736d933b7805ee867
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-07 20:20:14 +0530

    Added Image Loading Utilities

--- src/Utils.py
+++ src/Utils.py
@@ -5,6 +5,8 @@ from tensorflow.keras.layers import (
     Conv2DTranspose,
     UpSampling2D
 )
+from tqdm import tqdm
+from glob import glob
 
 
 class Utils:
@@ -64,3 +66,27 @@ class Utils:
         x = BatchNormalization(momentum = 0.5)(x)
         x = LeakyReLU(alpha = 0.2)(x)
         return x
+
+    @staticmethod
+    def normalize(image):
+        return (image.astype(np.float32) - 127.5) / 127.5
+
+    @staticmethod
+    def denormalize(image):
+        image = (image + 1) * 127.5
+        return image.astype(np.uint8)
+
+    @staticmethod
+    def load_data(data_directory, n_images):
+        image_paths = glob(data_directory + '/*')
+        x_train, y_train = [], []
+        for i in tqdm(range(n_images)):
+            image = cv2.imread(image_paths[i])
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = normalize(cv2.resize(image, (384, 384)))
+            x_train.append(image)
+        x_train = np.array(x_train)
+        for image in tqdm(x_train):
+            y_train.append(cv2.resize(image, (96, 96)))
+        y_train = np.array(y_train)
+        return x_train, y_train
