25bbfc1755d0d6ed4588afa8ea55d7ad3422bf89
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-07 15:33:13 +0530

    Minor Bug Fixes

--- src/Utils.py
+++ src/Utils.py
@@ -2,7 +2,8 @@ from tensorflow.keras.layers import (
     Conv2D, add,
     BatchNormalization,
     PReLU, LeakyReLU,
-    Conv2DTranspose
+    Conv2DTranspose,
+    UpSampling2D
 )
 
 
@@ -10,12 +11,13 @@ class Utils:
 
     @staticmethod
     def residual_block(x, filters, kernel_size, strides):
-        skip = x
+        x_shortcut = x
+
         x = Conv2D(
             filters = filters,
             kernel_size = kernel_size,
             strides = strides,
-            padding = 'same'
+            padding = "same"
         )(x)
         x = BatchNormalization(momentum = 0.5)(x)
         x = PReLU(
@@ -24,25 +26,29 @@ class Utils:
             alpha_constraint = None,
             shared_axes = [1, 2]
         )(x)
+        
         x = Conv2D(
             filters = filters,
             kernel_size = kernel_size,
             strides = strides,
-            padding = 'same'
+            padding = "same"
         )(x)
         x = BatchNormalization(momentum = 0.5)(x)
-        x = add([skip, x])
-        return x
+
+        output = add([x_shortcut, x])
+
+        return output
     
 
     @staticmethod
     def upsampling_block(x, filters, kernel_size, strides):
-        x = Conv2DTranspose(
+        x = Conv2D(
             filters = filters,
             kernel_size = kernel_size,
             strides = strides,
-            padding = 'same'
+            padding = "same"
         )(x)
+        x = UpSampling2D(size = 2)(x)
         x = LeakyReLU(alpha = 0.2)(x)
         return x
     
